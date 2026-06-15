579eb32945c738466f691dd03458aca8f181743a
Asura <asurahurbanova@gmail.com>
2026-06-15 16:33:35 +0200

    fix [id] page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from '@/app/notes/[id]/NoteDetails.client';
+import NoteDetailsClient from './NoteDetails.client';
 
-export default async function NoteDetailsPage({ params }: { params: { id: string } }) {
+export default async function NoteDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['note', params.id],
-    queryFn: () => fetchNoteById(params.id),
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
   });
 
   return (
